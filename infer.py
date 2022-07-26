8ecb2ad2d8b63742f0b9ce78212257d177a8deed
vietawake <vietawake@gmail.com>
2022-07-26 21:19:23 +0700

    add infer event importance

--- infer.py
+++ infer.py
@@ -1,4 +1,3 @@
-from fastcore.basics import true
 import torch
 import argparse
 import os
@@ -6,8 +5,7 @@ import matplotlib.pyplot as plt
 import torchvision.utils
 from PIL import Image
 import numpy as np
-from models import create_model
-from utils.utils import create_dataloader, validate
+from models.models import MTResnetAggregate
 
 
 # ----------------------------------------------------------------------
@@ -15,34 +13,42 @@ from utils.utils import create_dataloader, validate
 parser = argparse.ArgumentParser(
     description='PETA: Photo album Event recognition using Transformers Attention.')
 parser.add_argument('--model_path', type=str,
-                    default='./peta_32.pth')
+                    default='/content/drive2/checkpoints/event_importance_tresnet/version_2/checkpoints/best-epoch=119-mAP=67.21.ckpt')
 parser.add_argument('--album_path', type=str,
-                    default='./albums/Graduation/0_92024390@N00')
-# /Graduation') # /0_92024390@N00')
+                    default='../CUFED_split/images/test/0_11202221@N00')
+parser.add_argument('--image_importance_pth', type=str,
+                    default='../CUFED_split/image_importance.json')
+parser.add_argument('--event_type_pth', type=str,
+                    default='../CUFED_split/event_type.json')
+parser.add_argument('---album_list', type=str,
+                    default='filenames/test.txt')
 parser.add_argument('--val_dir', type=str, default='./albums')
 parser.add_argument('--num_classes', type=int, default=23)
 parser.add_argument('--model_name', type=str, default='mtresnetaggregate')
-parser.add_argument('--transformers_pos', type=int, default=1)
+parser.add_argument('--transformers_pos', type=int, default=0)
 parser.add_argument('--input_size', type=int, default=224)
 parser.add_argument('--transform_type', type=str, default='squish')
+parser.add_argument('--backbone', type=str, default='tresnet_m')
 parser.add_argument('--album_sample', type=str, default='rand_permute')
 parser.add_argument('--dataset_path', type=str, default='./data/ML_CUFED')
 parser.add_argument('--dataset_type', type=str, default='ML_CUFED')
 parser.add_argument('--path_output', type=str, default='./outputs')
 parser.add_argument('--use_transformer', type=int, default=1)
+parser.add_argument('--infer', type=int, default=1)
 parser.add_argument('--album_clip_length', type=int, default=32)
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--num_workers', type=int, default=0)
 parser.add_argument('--top_k', type=int, default=3)
-parser.add_argument('--threshold', type=float, default=0.85)
+parser.add_argument('--threshold', type=float, default=0.5)
 parser.add_argument('--remove_model_jit', type=int, default=None)
+parser.add_argument('--attention', type=str,
+                    default='multihead')
 
 
 def get_album(args):
 
     files = os.listdir(args.album_path)
     n_files = len(files)
-    print(n_files)
     idx_fetch = np.linspace(0, n_files-1, args.album_clip_length, dtype=int)
     tensor_batch = torch.zeros(
         len(idx_fetch), args.input_size, args.input_size, 3)
@@ -56,24 +62,17 @@ def get_album(args):
     montage = torchvision.utils.make_grid(tensor_batch).permute(1, 2, 0).cpu()
     return tensor_batch, montage
 
-    
-
 
 def inference(tensor_batch, model, classes_list, args):
-
-    out = model(tensor_batch)
-    print(out.shape)
-    output = torch.squeeze(torch.sigmoid(model(tensor_batch)))
-    print(output)
+    output, _ = model(tensor_batch)
+    output = torch.squeeze(torch.sigmoid(output))
     np_output = output.cpu().detach().numpy()
     idx_sort = np.argsort(-np_output)
     # Top-k
     detected_classes = np.array(classes_list)[idx_sort][: args.top_k]
     scores = np_output[idx_sort][: args.top_k]
-
     # Threshold
     idx_th = scores > args.threshold
-    print(detected_classes[idx_th], scores[idx_th])
     return detected_classes[idx_th], scores[idx_th]
 
 
@@ -91,41 +90,37 @@ def display_image(im, tags, filename, path_dest):
     plt.savefig(os.path.join(path_dest, filename))
 
 
-def main():
-    print('PETA demo of inference code on a single album.')
+def load_model(net, path):
+    if path is not None and path.endswith(".ckpt"):
+        state_dict = torch.load(path, map_location='cpu')
+        if "state_dict" in state_dict:
+            state_dict = state_dict["state_dict"]
+        compatible_state_dict = {}
+        for k, v in state_dict.items():
+            if k.startswith('net.'):
+                compatible_state_dict[k.replace('net.', '')] = v
+        net.load_state_dict(compatible_state_dict)
 
-    # ----------------------------------------------------------------------
-    # Preliminaries
+    return net
+
+
+def main(classes_list):
     args = parser.parse_args()
-
-    # Setup model
-    print('creating and loading the model...')
-    state = torch.load(args.model_path, map_location='cpu')
-    # args.num_classes = state['num_classes']
-    model = create_model(args).cuda()
-    model.load_state_dict(state['model'], strict=True)
-    model.eval()
-    classes_list = np.array(list(state['idx_to_class'].values()))
-    print('Class list:', classes_list)
-
-    # Setup data loader
-    print('creating data loader...')
-    val_loader = create_dataloader(args)
-    for i, (images, labels) in enumerate(val_loader):
-        print(images.shape)
-        print(labels.shape)
-    print('done\n')
+    # net = EventNetwork(encoder_name='resnet101', num_classes=args.num_classes).cuda()
+    # net.eval()
+    net = MTResnetAggregate(args).cuda()
+    net.eval()
+    net = load_model(net, args.model_path)
 
     # Get album
     tensor_batch, montage = get_album(args)
 
     # Inference
-    tags, confs = inference(tensor_batch, model, classes_list, args)
-    print(montage.shape)
+    tags, confs = inference(tensor_batch, net, classes_list, args)
 
     # Visualization
     display_image(montage, tags, 'result.jpg', os.path.join(
-        args.path_output, args.album_path).replace("./albums", ""))
+        args.path_output, args.album_path.rsplit('/')[1]).replace("./albums", ""))
 
     # Actual validation process
     # print('loading album and doing inference...')
@@ -136,15 +131,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
-#     args = parser.parse_args()
-#     from models.backbone.resnet import resnet34
-#     from models.clsnetwork import EventNetwork
-#     import torch.nn as nn
-#     # Setup model
-#     print('creating and loading the model...')
-#     # model = create_model(args).cuda()
-#     m = EventNetwork(encoder_name='resnet34', num_classes=23).cuda()
-#     x = torch.randn(56, 3, 224, 224).cuda()
-#     o = m(x)
-#     print(o.shape)
+    classes_list = ['Architecture', 'BeachTrip', 'Birthday', 'BusinessActivity',
+                    'CasualFamilyGather', 'Christmas', 'Cruise', 'Graduation', 'GroupActivity',
+                    'Halloween', 'Museum', 'NatureTrip', 'PersonalArtActivity',
+                    'PersonalMusicActivity', 'PersonalSports', 'Protest', 'ReligiousActivity',
+                    'Show', 'Sports', 'ThemePark', 'UrbanTrip', 'Wedding', 'Zoo']
+    main(classes_list)
